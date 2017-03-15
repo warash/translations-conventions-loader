2e8d140da2fc8a882c92b0d94ca9304a13091b5a
DOMAIN\michal.majewski <majewski9@gmail.com>
2017-03-15 12:02:57 +0100

    adding versioning to i18n files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var loaderUtils = require('loader-utils');
 var fs = require('fs');
 var path = require('path');
-
+var glob = require("glob")
 var translationRegex = /@Translations\({[\s]*?}\)$/gm;
 
 
@@ -22,24 +22,42 @@ function TranslationsConventionsLoader(source, sourcemap) {
 
     source = source.replace(translationRegex, function (match, offset, src) {
 
+
         var fileContext = self.request.split(self.context);
+
+
         var lastFileName = fileContext[fileContext.length - 1];
-        lastFileName = lastFileName.replace(/^.*[\\\/]/, '').replace(/\.[^.]*$/, '');
+        lastFileName = lastFileName.replace(/^.*[\\\/]/, '').replace(/\.[^.]*$/, '') +".i18n.json";
         // if (lastFileName.indexOf(relativePathStart) === 0) {
         //     lastFileName = lastFileName.substr(relativePathStart.length);
         // }
 
+        // var translationFiles = glob.sync('lastFileName*.i18n.json');
+        // path.join(self.context, lastFileName + cssExtension))
+
+
+        var tranlationFileWildCard = path.join(self.context, lastFileName.replace(".i18n.json", "*.i18n.json"));
+        var translationFiles = glob.sync(tranlationFileWildCard);
+        console.log(translationFiles);
+        if (translationFiles.length) {
+            var last = translationFiles[translationFiles.length - 1];
+            // var lastPath = last.split("/");
+            //
+            // lastFileName = lastPath[lastPath.length - 1];
+            lastFileName = path.basename(last);
+        }
+
+
         var metadata;
-        if (query.remote){
-            metadata = "{ remote: './" + lastFileName + ".i18n.json' }";
-        }else{
-            metadata = "{ remote: './" + lastFileName + ".i18n.json', static: { 'en': require('./" + lastFileName + ".i18n.json') } }";
+        if (query.remote) {
+            metadata = "{ remote: './" + lastFileName + "' }";
+        } else {
+            metadata = "{ remote: './" + lastFileName + "', static: { 'en': require('./" + lastFileName + "') } }";
         }
 
 
         return '@Translations' + '(' + metadata + ')';
     });
-
 
 
     if (self.callback) {
